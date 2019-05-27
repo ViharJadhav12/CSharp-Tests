dbd75428af1c32b3485f1b06e460fdf1eb4dd9e3
Sandip Nalwade <sandipn@alohatechnology.com>
2019-05-27 15:25:06 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+
+        }
+    }
+
+    class Test
+    {
+        readonly int i = 10;
+        readonly List<int> intList = new List<int>() { 10,20,30};
+
+        public Test()
+        {
+            intList = new List<int>() {30,40,50};
+        }
+
+        private void TestMethod()
+        {
+            //intList = new List<int>();
+        }
+
+    }
+}
